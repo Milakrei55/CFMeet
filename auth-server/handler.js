a53c9b77af9d5b46e92c6004cf2a6f718ed69a32
Milakrei55 <mischakom@gmail.com>
2025-04-26 18:46:16 +0200

    new vercel app update

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -3,9 +3,7 @@ const calendar = google.calendar("v3");
 const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
 
-const redirect_uris = [
-  "https://XXXXX.vercel.app"
-];
+const redirect_uris = ["https://cf-meet-three.vercel.app"];
 
 const oAuth2Client = new google.auth.OAuth2(
   CLIENT_ID,
